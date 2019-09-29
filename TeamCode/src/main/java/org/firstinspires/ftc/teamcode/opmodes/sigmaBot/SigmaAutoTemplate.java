1d328333db5bfab6bcf53bfacce947cc470bdc10
Your Name <you@example.com>
2019-09-28 18:43:04 -0700

    Change foundation hook position and turn up auto program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/sigmaBot/SigmaAutoTemplate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/sigmaBot/SigmaAutoTemplate.java
@@ -30,7 +30,6 @@ public class SigmaAutoTemplate extends LinearOpMode {
     private Configuration configuration;
     private Logger<Logger> log = new Logger<Logger>().configureLogging(getClass().getSimpleName(), LOG_LEVEL);
 
-    public SwerveChassis chassis;
     public double auto_chassis_power = .3;
 
     @Override
@@ -61,7 +60,7 @@ public class SigmaAutoTemplate extends LinearOpMode {
         if (robot.cameraStoneDetector.getTfod()!=null) {
             updatedRecognitions = robot.cameraStoneDetector.getTfod().getUpdatedRecognitions();
         }
-        int robot_pos = 2;
+        int robot_pos = 1;
 
         waitForStart();
         robot.runtime.reset();
@@ -69,21 +68,21 @@ public class SigmaAutoTemplate extends LinearOpMode {
         if (opModeIsActive()) {
             try {
                 // put autonomous steps here
-                // step-1: detect skystone location\
+                // step-1: detect skystone location
                 StoneLoc = robot.cameraStoneDetector.getSkystonePositionTF(robot_pos);
-                telemetry.addLine(StoneLoc.toString());
-                telemetry.update();
-                sleep(10000); // 10 sec
-                // step-2: go to gran the first skystone
-                // step-3: deliver the first skystone
-                //
-//                int skyStonePosition = 3; // Collin, this is for your function
-//                getFirstSkyStone(StoneLoc);
-//                int count = 1;
-//                while (getRuntime() < 25000){
-//                    count++;
-//                    getAnotherSkyStone(skyStonePosition, count);
-//                }
+                // telemetry.addLine(StoneLoc.toString());
+                // telemetry.update();
+                // sleep(10000); // 10 sec
+
+                // step-2: go to grab the first skystone and deliver
+                robot.getFirstSkyStone(StoneLoc);
+                int count = 1;
+
+                // step-3: grab and deliver the next skystone/stone
+                if (getRuntime() < 250000){
+                    count++;
+                    robot.getAnotherSkyStone(StoneLoc, count);
+                }
                 // move foundation
                 // park
 
@@ -94,44 +93,7 @@ public class SigmaAutoTemplate extends LinearOpMode {
             }
         }
     }
-    public void getFirstSkyStone(ToboSigma.SkystoneLocation skyStonePosition) throws InterruptedException{
-        chassis.driveStraightAuto(auto_chassis_power, 65, 0, 10000);
-        if(skyStonePosition == ToboSigma.SkystoneLocation.LEFT){
-            chassis.driveStraightAuto(auto_chassis_power, 1, 90, 10000);  // test to get exact numbers
-        } else if(skyStonePosition == ToboSigma.SkystoneLocation.CENTER){
-            chassis.driveStraightAuto(auto_chassis_power, 1, 90, 10000);  // test to get exact numbers
-        } else if(skyStonePosition == ToboSigma.SkystoneLocation.RIGHT) {
-            chassis.driveStraightAuto(auto_chassis_power, 1, 90, 10000);  // test to get exact numbers
-        }
-        //grab skystone
-        int ss_pos = 1;
-        if (skyStonePosition== ToboSigma.SkystoneLocation.RIGHT)
-            ss_pos = 3;
-        else if (skyStonePosition== ToboSigma.SkystoneLocation.CENTER)
-            ss_pos = 2;
-        chassis.driveStraightAuto(auto_chassis_power, 220 + 20 * ss_pos, -90, 15000);//probably too much
-        //place skystone
-    }
-    public void getAnotherSkyStone(int skyStonePosition, int stoneNum) throws InterruptedException{//stoneNum - how many stones ara we going to have after this trip
-        int toTake;
-        if (skyStonePosition == 1) {
-           int [] a = {4, 2, 3, 5, 6};
-           toTake = a[stoneNum - 2];
-        } else if (skyStonePosition == 2) {
-            int [] a = {5, 1, 3, 4, 6};
-            toTake = a[stoneNum - 2];
-        } else {
-            int[] a = {6, 1, 2, 4, 5};
-            toTake = a[stoneNum - 2];
-        }
-        chassis.driveStraightAuto(auto_chassis_power, 260 + 20 * stoneNum, 90, 15000);//numbers - probably not correct
-        chassis.driveStraightAuto(auto_chassis_power, 20, 0, 10000);
-        chassis.driveStraightAuto(auto_chassis_power, -5, 0, 10000);
-        //grab stone
-        chassis.driveStraightAuto(auto_chassis_power, 243 + 20 * stoneNum, -90, 15000);
-        // place stone on foundation
 
-    }
     protected void handleException(Throwable T) {
         log.error(T.getMessage(), T);
         int linesToShow = 5;
