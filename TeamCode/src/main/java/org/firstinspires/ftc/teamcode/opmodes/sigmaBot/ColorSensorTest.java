64f24154d1c6f62e0e4e27b37a0846f5c5cfd38f
Collin Zhong <zhong.collin@gmail.com>
2020-02-04 22:06:03 -0800

    disable color test program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/sigmaBot/ColorSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/sigmaBot/ColorSensorTest.java
@@ -51,6 +51,7 @@ import com.qualcomm.robotcore.hardware.SwitchableLight;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
+@Disabled
 @TeleOp(name = "Sensor: Color", group = "Sensor")
 public class ColorSensorTest extends LinearOpMode {
 
